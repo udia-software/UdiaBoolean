00b13fb13d943b453f924d683fa137d3f7e85640
Alexander Wong <admin@alexander-wong.com>
2016-08-23 02:19:13 -0600

    Commented tests. Added .idea to gitignore.

--- test.js
+++ test.js
@@ -1,21 +1,25 @@
 'use strict';
 
+var udia = {};
 var test = require('tape');
-var boolean = require('./');
 
 test(function(t) {
-  t.plan(3);
+  udia['boolean'] = require('./');
 
-  t.equal(boolean.t(), true);
-  t.equal(boolean.f(), false);
+  t.plan(3);  // There will be three tests.
 
-  var maybe = boolean.m();
-  if (maybe === boolean.t()) {
-    t.equal(maybe, true);
-  } else if (maybe === boolean.f()) {
-    t.equal(maybe, false);
+  t.equal(udia['boolean'].t(), true);         // Is udia true equal to true?
+  t.equal(udia['boolean'].f(), false);        // Is udia false equal to false?
+
+  var maybe = udia['boolean'].m();            // What is udia?
+
+  if (maybe === udia['boolean'].t()) {        // Is udia equal to udia true?
+    t.equal(maybe, true);                     //  - If so, is udia equal to true?
+  } else if (maybe === udia['boolean'].f()) { // Otherwise is udia equal to udia false?
+    t.equal(maybe, false);                    // - If so, is udia equal to false?
   } else {
-    t.equal(false, true);
+    t.equal(false, true);                     // Otherwise is false equal to true?
   }
-  t.end();
+  t.end();                                    // Udia is defined.
+
 });
